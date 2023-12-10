98fe6deb1c3a6ce00521873e96120f294d77e898
Alexander Winkowski <dereference23@outlook.com>
2023-12-10 06:26:26 +0000

    techpack: display: Reset panel on the first kickoff
    It looks like the panel boots in a state different from its "normal"
    one. This is noticable when comparing colorful images before and after
    the first panel power cycle. As a workaround, trigger ESD recovery
    on the first kickoff to bring the panel into the "normal" state.
    
    Change-Id: Ic1ca60d4da5d57ef9de40e79e8fba7e53d6ee962
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- techpack/display/msm/sde/sde_connector.c
+++ techpack/display/msm/sde/sde_connector.c
@@ -2886,8 +2886,8 @@ static int sde_connector_atomic_check(struct drm_connector *connector,
 extern void set_esd_check_happened(int val);
 #endif
 
-static void _sde_connector_report_panel_dead(struct sde_connector *conn,
-	bool skip_pre_kickoff)
+void _sde_connector_report_panel_dead(struct sde_connector *conn,
+				      bool skip_pre_kickoff)
 {
 	struct drm_event event;
 
--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -4854,6 +4854,8 @@ static int _sde_encoder_reset_ctl_hw(struct drm_encoder *drm_enc)
 void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error,
 		bool config_changed)
 {
+	static bool has_run;
+	struct sde_connector *sde_conn;
 	struct sde_encoder_virt *sde_enc;
 	struct sde_encoder_phys *phys;
 	unsigned int i;
@@ -4915,6 +4917,8 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error,
 		SDE_EVT32(DRMID(drm_enc), i, SDE_EVTLOG_FUNC_CASE1);
 	}
 
+	sde_conn = to_sde_connector(sde_enc->cur_master->connector);
+
 #ifdef OPLUS_BUG_STABILITY
 /* Add for backlight smooths */
 	if (sde_enc && sde_enc->cur_master && sde_enc->cur_master->connector) {
@@ -4950,6 +4954,12 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error,
 	iris_sde_encoder_sync_panel_brightness(sde_enc->num_phys_encs,
 			sde_enc->phys_encs[0]);
 #endif
+
+	if (unlikely(!has_run)) {
+		has_run = true;
+		_sde_connector_report_panel_dead(sde_conn, false);
+	}
+
 	SDE_ATRACE_END("encoder_kickoff");
 #ifdef OPLUS_BUG_STABILITY
 	sde_connector_update_backlight(sde_enc->cur_master->connector, true);
