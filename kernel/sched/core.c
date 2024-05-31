c8e90ff1e8bd5fa25fc2a1af29eb594d72fbd2f2
Dark-Matter7232 <me@const.eu.org>
2024-05-31 13:29:49 +0530

    Revert "sched: Improve the scheduler"
    This reverts commit b1250b5cf67e82c7607a3989a475d96d2cfc5c74.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5839,7 +5839,7 @@ bool is_sched_lib_based_app(pid_t pid)
 	char *libname, *lib_list;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
-	char *tmp_lib_name;
+	char tmp_lib_name[LIB_PATH_LENGTH];
 	bool found = false;
 	struct task_struct *p;
 	struct mm_struct *mm;
@@ -5847,16 +5847,11 @@ bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
-	tmp_lib_name = kmalloc(LIB_PATH_LENGTH, GFP_KERNEL);
-	if (!tmp_lib_name)
-		return false;
-
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
-		kfree(tmp_lib_name);
 		return false;
 	}
 
@@ -5894,7 +5889,6 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
-	kfree(tmp_lib_name);
 	return found;
 }
 
